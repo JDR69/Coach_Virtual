fc97a8bef3211a1c31edec12f4811c298295ac0b
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-11-21 12:46:41 -0400

    api

--- coachvirtualfront/src/api/api.js
+++ coachvirtualfront/src/api/api.js
@@ -1,7 +1,7 @@
 // src/api/api.js
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://coach-virtual.onrender.com";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://coach-virtual.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
