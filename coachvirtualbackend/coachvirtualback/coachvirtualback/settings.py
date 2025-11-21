7c3d1e4321969f8f57786dc20fc17362b6cfad36
Joan Dr <joandanielrr@gmail.com>
2025-11-21 10:59:43 -0400

    Hosts

--- coachvirtualbackend/coachvirtualback/coachvirtualback/settings.py
+++ coachvirtualbackend/coachvirtualback/coachvirtualback/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-rn-ljf$%9a8^1+dsn&_^7_@5w-rajoub0h$m2q18g=glhw^blu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "192.168.1.2", "192.168.1.5"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
